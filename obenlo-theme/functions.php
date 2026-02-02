a845b3381a92d5a6df5694c1a63d68a1a288c164
Olouis <olouisbiz@gmail.com>
2026-02-01 22:08:39 -0500

    Delete obenlo-theme directory

--- obenlo-theme/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** Obenlo Marketplace Functions **/
-add_action('wp_enqueue_scripts', function() {
-    wp_enqueue_style('obenlo-style', get_stylesheet_uri(), [], time());
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Outfit:wght@400;900&display=swap', [], null);
-});
-
-add_theme_support('post-thumbnails');
-add_theme_support('title-tag');
--- obenlo-theme/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Obenlo Marketplace - Safety Fallback
- * Redirects unauthorized or lost traffic to the primary Marketplace Explorer.
- */
-wp_redirect(home_url('/listings'));
-exit;
